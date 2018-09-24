b4f1ea3a91512a5d44d05fe9c2a8ad15747f5ae5
jakirmithunbd <jakirmithunbd@gmail.com>
2018-09-24 12:44:22 +0600

    variable tutorial

---
+++ variable.php
@@ -0,0 +1,16 @@
+<?php
+// variable variable
+$name = "jakir";
+$$name = 'Bari Nai';
+// echo $jakir;
+
+
+$hello = "Hollo ";
+$bang  = "Bangladesh";
+// echo "{$hello} {$bang}";
+// echo "\n"; // line break in php
+// echo "{$hello} {$bang}";
+
+// Define const in php
+define('PI', 3.14159);
+echo PI;
