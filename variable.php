284b19c7337d907b21eeb9b83f6cf8e5446f9ce6
jakirmithunbd <jakirmithunbd@gmail.com>
2018-10-01 11:03:12 +0600

    number variable

--- variable.php
+++ variable.php
@@ -13,4 +13,24 @@ $bang  = "Bangladesh";
 
 // Define const in php
 define('PI', 3.14159);
-echo PI;
+// echo PI;
+
+// number 
+
+function getNumber($arr) {
+    $number = $arr;
+    printf('%s', $number);
+}
+// echo getNumber(010);
+
+$fname = 'jakirul';
+$lname = 'Islam';
+// printf('The name is %2$s and %1$s', $fname, $lname);
+
+$n = 12.453;
+// printf("%.2f", $n);
+
+$na = 123;
+$ma = 23;
+// printf("%05d", $na);
+// printf('%05d', $ma);
